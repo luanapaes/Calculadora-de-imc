a4387c2b8f37aa620e672cd185956e07a59c7ede
Luana Paes <luanafranca598@gmail.com>
2023-07-29 23:14:56 -0300

    files

---
+++ main.js
@@ -0,0 +1,39 @@
+var classificacao = document.querySelector('#classificacao');
+
+var txtPeso = (document.querySelector('#peso'));
+var txtAltura = (document.querySelector('#altura'));
+
+function calcularImc() {
+    var peso = parseFloat(txtPeso.value);
+    var altura = parseFloat(txtAltura.value);
+
+    if (isNaN(peso) || isNaN(altura)) {
+        document.querySelector('#output').innerHTML =
+            'Informe seu peso e altura em números, não deixe os espaços vazios.';
+    } else {
+        imc = parseFloat(peso / (altura * altura)).toFixed(2);
+        document.querySelector('#output').innerHTML =
+            'Seu IMC: ' + imc;
+
+        classificarImc(imc);
+    }
+}
+
+function classificarImc(imc) {
+    if (imc <= 16.9) {
+        classificacao.innerHTML = 'Classificação: Muito abaixo do peso';
+    } else if (imc <= 18.4) {
+        classificacao.innerHTML = 'Classificação: Abaixo do peso';
+    } else if (imc <= 24.9) {
+        classificacao.innerHTML = 'Classificação: Normal';
+    } else if (imc <= 29.9) {
+        classificacao.innerHTML = 'Classificação: acima do peso';
+    } else if (imc <= 34.9) {
+        classificacao.innerHTML = 'Classificação: Obesidade I';
+    } else if (imc <= 40) {
+        classificacao.innerHTML = 'Classificação: Obesidade II';
+
+    } else {
+        classificacao.innerHTML = 'Classificação: Obesidade III';
+    }
+}
